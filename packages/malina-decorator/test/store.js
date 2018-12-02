f255b5efa0d3cd6b0699eb274261498d873541f7
vacavaca <vacavaca@fastmail.com>
2018-12-02 03:14:05 +0300

    Init tests

--- packages/malina-decorator/test/store.js
+++ packages/malina-decorator/test/store.js
@@ -4,12 +4,24 @@ const { JSDOM } = require('jsdom')
 const { h, view, mount } = require('malina')
 const { withStore, connect, bindActions } = require('..')
 
-const barier = () => {
-  const result = {}
-  result.lock = new Promise(resolve => {
-    result.unlock = resolve
+const asyncTest = test => async () => {
+  let queue = []
+  let wake = null
+  let lock = new Promise(resolve => { wake = resolve })
+
+  test(fn => (...args) => {
+    queue.push(fn(...args))
+    if (wake != null)
+      wake()
   })
-  return result
+
+  await lock
+  wake = null
+
+  while (queue.length > 0) {
+    await Promise.all(queue)
+    queue = []
+  }
 }
 
 describe('store', () => {
@@ -17,10 +29,10 @@ describe('store', () => {
     it('should initialize view', () => {
       const dom = new JSDOM('<body></body>')
 
-      // const Test = withStore({})(view((s, a, children) => children))
-      // mount(dom.window.document.body, h(Test, {}, ['Hello']))
+      const Test = withStore({})(view((s, a, children) => children))
+      mount(dom.window.document.body, h(Test, {}, ['Hello']))
 
-      // assert.strictEqual(dom.window.document.body.innerHTML, 'Hello')
+      assert.strictEqual(dom.window.document.body.innerHTML, 'Hello')
     })
   })
 
@@ -28,45 +40,35 @@ describe('store', () => {
     it('should pass the store around', () => {
       const dom = new JSDOM('<body></body>')
 
-      // const Bottom = connect(store => ({ store }))(view(({ store }) => `value: ${store.foo}`))
-      // const Middle = view((s, a, children) => h('div', {}, [h(Bottom)]))
-      // const Top = withStore({ foo: 42 })(view((s, a, children) => h(Middle)))
+      const Bottom = connect(store => ({ store }))(view(({ store }) => `value: ${store.foo}`))
+      const Middle = view((s, a, children) => h('div', {}, [h(Bottom)]))
+      const Top = withStore({ foo: 42 })(view((s, a, children) => h(Middle)))
 
-      // mount(dom.window.document.body, h(Top))
+      mount(dom.window.document.body, h(Top))
 
-      // assert.strictEqual(dom.window.document.body.innerHTML, '<div>value: 42</div>')
+      assert.strictEqual(dom.window.document.body.innerHTML, '<div>value: 42</div>')
     })
 
-    it('should update store properly', async () => {
+    it('should update store properly', asyncTest(async track => {
       const dom = new JSDOM('<body></body>')
 
-      // let created = barier()
-      // let updated = barier()
+      const actions = {}
+      actions.update = increment => store =>
+        ({ foo: store.foo + increment })
 
-      // const actions = {}
-      // actions.update = tempo => store =>
-      //   ({
-      //     foo: store.foo++
-      //   })
+      const hooks = {}
+      hooks.mount = track(async (mount, { update }) => {
+        assert.strictEqual(dom.window.document.body.innerHTML, '<div>value: 42</div>')
+        await update(3)
+        assert.strictEqual(dom.window.document.body.innerHTML, '<div>value: 45</div>')
+      })
 
-      // const hooks = {}
-      // hooks.create = (mount, { update }) => {
-      //   created.unlock()
-      //   update()
-      //   updated.unlock()
-      // }
+      const Bottom = view(({ store }) => `value: ${store.foo}`, {}, {}, hooks)
+      const ConnectedBottom = connect(store => ({ store }), bindActions(actions))(Bottom)
+      const Middle = view((s, a, children) => h('div', {}, [h(ConnectedBottom)]))
+      const Top = withStore({ foo: 42 })(view((s, a, children) => h(Middle)))
 
-      // const Bottom = view(({ store }) => `value: ${store.foo}`, {}, {}, hooks)
-      // const ConnectedBottom = connect(store => ({ store }), bindActions(actions))(Bottom)
-
-      // const Middle = view((s, a, children) => h('div', {}, [h(ConnectedBottom)]))
-      // const Top = withStore({ foo: 42 })(view((s, a, children) => h(Middle)))
-
-      // mount(dom.window.document.body, h(Top))
-
-      // await created.lock
-
-      // assert.strictEqual(dom.window.document.body.innerHTML, '<div>value: 42</div>')
-    })
+      mount(dom.window.document.body, h(Top))
+    }))
   })
 })
